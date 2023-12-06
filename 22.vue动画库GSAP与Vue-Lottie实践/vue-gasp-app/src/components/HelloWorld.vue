9dcdf9d1245f0757c4d65926c24bc675d2bbef86
wangliang4 <wangliang4@newhope.cn>
2023-12-06 16:11:14 +0800

    feat: 新增日常使用框架大合集

---
+++ 22.vue动画库GSAP与Vue-Lottie实践/vue-gasp-app/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue'
+import gsap from 'gsap'
+
+
+onMounted(() => {
+  gsap.to('.box', {
+    x: 200,
+    duration: 2,
+    // ease: 'elastic',
+    rotation: 360,
+  })
+})
+
+
+</script>
+
+<template>
+  <div class="box"></div>
+</template>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  border-radius: 4px;
+}
+</style>
