f59177ea6a69bae54a78d682be86b4c9903f6d45
wangliang4 <wangliang4@newhope.cn>
2023-12-19 20:26:04 +0800

    feat: 更新框架内容

---
+++ 27.利用gsap实现数字滚动效果/gsap-count-to/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld :to="num"/>
+
+  <button @click="num += 10">加10</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const num = ref(10)
+
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ 27.利用gsap实现数字滚动效果/gsap-count-to/src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<script setup >
+import { reactive, onMounted, watchEffect } from 'vue'
+import gsap from 'gsap'
+
+
+// const props = withDefaults(
+//   defineProps<{
+//     from: number
+//     to: number
+//     duration?: number
+//   }>(),
+//   {
+//     from: 0,
+//     to: 0,
+//     duration: 1
+//   }
+// )
+
+// 定义 props
+const props = defineProps({
+  from: {
+    type: Number,
+    default: 0
+  },
+  to: {
+    type: Number,
+    default: 100
+  },
+  duration: {
+    type: Number,
+    default: 2
+  }
+});
+
+
+let tweened = reactive({
+  number: 0
+})
+
+watchEffect(() => {
+  gsap.to(tweened, {
+    number: props.to.toFixed(0),
+    duration: props.duration,
+    ease: 'power1.out',
+  })
+})
+
+</script>
+
+<template>
+  <div class="gsap">
+    {{
+      tweened.number.toFixed(0)
+    }}
+  </div>
+</template>
+
+<style scoped>
+.gsap {
+  width: 200px;
+  height: 100px;
+  background-color: #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 20px;
+
+}
+</style>
