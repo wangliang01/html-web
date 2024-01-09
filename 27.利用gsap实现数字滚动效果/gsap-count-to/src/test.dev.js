490cfeb569701fb8ade5cd0a6bb40423d07e96c0
王亮 <wangliang4@newhope.cn>
2024-01-09 08:52:39 +0800

    feat: 表单进阶

---
+++ 27.利用gsap实现数字滚动效果/gsap-count-to/src/test.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// 帮我写一个求和函数 
+function sum(a, b) {
+  return a + b;
+}
---
+++ 27.利用gsap实现数字滚动效果/gsap-count-to/src/test.js
@@ -0,0 +1,11 @@
+// 帮我写一个求和函数 
+function sum(a, b) {
+  return a + b;
+}
+
+// 或者使用ES6箭头函数
+const sum = (a, b) => a + b;
+
+// 调用函数
+console.log(sum(5, 3)); // 输出 8
+console.log(sum(10, 20)); // 输出 30
