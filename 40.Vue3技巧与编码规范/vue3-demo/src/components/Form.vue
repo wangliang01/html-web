3442bc016003ff626c6356aa853460566e311799
wangliang4 <wangliang4@newhope.cn>
2024-08-23 15:18:03 +0800

    feat: Vue3技巧

---
+++ 40.Vue3技巧与编码规范/vue3-demo/src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <UserName v-model:first-name="firstName" v-model:last-name="lastName"></UserName>
+  <p>{{ firstName }} - {{ lastName }}</p>
+</template>
+
+<script setup>
+import { ref, provide } from 'vue'
+import UserName from './UserName.vue'
+
+const form = ref({
+  age: 30,
+  email: '123@qq.com',
+})
+
+// 将form信息provide子组件
+provide('form', form)
+
+const firstName = ref('')
+const lastName = ref('')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 40.Vue3技巧与编码规范/vue3-demo/src/components/UserName.vue
@@ -0,0 +1,18 @@
+<template>
+  <input type="text" v-model="firstName">
+  <input type="text" v-model="lastName">
+  <div>其他信息:
+    <p>年龄:{{ form.age }} </p>
+    <p>邮箱:{{ form.email }}</p>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+const firstName = defineModel('firstName')
+const lastName = defineModel('lastName')
+
+const form = inject('form')
+</script>
+
+<style lang="scss" scoped></style>
